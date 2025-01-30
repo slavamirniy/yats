eb61e2fdef237a676891f82279504c71e75c3574
Your Name <you@example.com>
2025-01-30 12:43:59 +0300

    гзв

--- src/workflowprovider.ts
+++ src/workflowprovider.ts
@@ -18,10 +18,10 @@ export class WorkflowSystemActivityProvider<T extends WorkflowSystem<any, any, a
         super();
     }
 
-    async getActivityResult<Name extends keyof T["data"]["workflows"]>(
+    async getActivityResult<Name extends keyof T["data"]["workflows"], ARG extends { workflowId: string } | { args: T["data"]["workflows"][Name]["in"] }>(
         activityname: Name,
-        arg: { workflowId: string } | { args: T["data"]["workflows"][Name]["in"] }
-    ): Promise<{ workflowId: string } | T["data"]["workflows"][Name]["out"]> {
+        arg: ARG
+    ): Promise<ARG extends { workflowId: string } ? T["data"]["workflows"][Name]["out"] : { workflowId: string }> {
         if ('args' in arg) {
             const data = await this.workflowSystem.execute(activityname, arg.args);
             return { workflowId: data.workflow_id };
