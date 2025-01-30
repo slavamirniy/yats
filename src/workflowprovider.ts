95427c90abf5619c58091df2fa3ea76f45f4a4ec
Your Name <you@example.com>
2025-01-30 12:50:24 +0300

    Enhance WorkflowSystemActivityProvider with branded workflow ID types and improved result handling

--- src/workflowprovider.ts
+++ src/workflowprovider.ts
@@ -3,31 +3,35 @@ import { IActivitesProvider, UnionToArray, WorkflowSystem } from "./base";
 export class WorkflowSystemActivityProvider<T extends WorkflowSystem<any, any, any, any>> extends IActivitesProvider<{
     [K in keyof T['data']['workflows']]: {
         in: { args: T['data']['workflows'][K]['in'] },
-        out: { workflowId: string },
+        out: { workflowId: string & { __brand: K } },
         additionalData: {}
     } |
     {
-        in: { workflowId: string },
-        out: T['data']['workflows'][K]['out'],
+        in: { workflowId: string & { __brand: K } },
+        out: { workflowId: string & { __brand: K }, result: T['data']['workflows'][K]['out'] },
         additionalData: T['data']['workflows'][K]['additionalData']
     }
 }> {
-    InferActivities!: { [K in keyof T["data"]["workflows"]]: { in: { args: T["data"]["workflows"][K]["in"]; }; out: { workflowId: string; }; additionalData: {}; } | { in: { workflowId: string; }; out: T["data"]["workflows"][K]["out"]; additionalData: T["data"]["workflows"][K]["additionalData"]; }; };
+    InferActivities!: { [K in keyof T["data"]["workflows"]]: { in: { args: T["data"]["workflows"][K]["in"]; }; out: { workflowId: string & { __brand: K; }; }; additionalData: {}; } | { in: { workflowId: string & { __brand: K; }; }; out: { workflowId: string & { __brand: K; }; result: T["data"]["workflows"][K]["out"]; }; additionalData: T["data"]["workflows"][K]["additionalData"]; }; };
 
     constructor(private workflowSystem: T) {
         super();
     }
 
-    async getActivityResult<Name extends keyof T["data"]["workflows"], ARG extends { workflowId: string } | { args: T["data"]["workflows"][Name]["in"] }>(
+    async getActivityResult<Name extends keyof T["data"]["workflows"]>(
         activityname: Name,
-        arg: ARG
-    ): Promise<ARG extends { workflowId: string } ? T["data"]["workflows"][Name]["out"] : { workflowId: string }> {
+        arg: { args: T['data']['workflows'][Name]['in'] } | { workflowId: string & { __brand: Name } }
+    ): Promise<T['data']['workflows'][Name]['out']> {
         if ('args' in arg) {
             const data = await this.workflowSystem.execute(activityname, arg.args);
-            return { workflowId: data.workflow_id };
+            return { workflowId: data.workflow_id as string & { __brand: Name } };
         }
-        return this.workflowSystem.getPromiseByWorkflowId(activityname, arg.workflowId);
+        return {
+            workflowId: arg.workflowId,
+            result: await this.workflowSystem.getPromiseByWorkflowId(activityname, arg.workflowId)
+        };
     }
+
 
     getActivitiesNames(): UnionToArray<keyof T["data"]["workflows"]> {
         throw new Error("Method not implemented.");
