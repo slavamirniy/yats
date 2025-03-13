6882814184462012e79fefcf1f925550a38e625c
Your Name <you@example.com>
2025-03-13 12:57:13 +0300

    fix

--- src/default.ts
+++ src/default.ts
@@ -119,9 +119,9 @@ export class ProtocolCollector<T extends Record<string, any>> {
         return new ProtocolCollector(provider);
     }
 
-    use<NEW_T extends Record<string, any>>(protocol: { setProvider: (provider: IActivitesProvider<T>) => IProtocolActivitiesProvider<NEW_T> }): ProtocolCollector<NEW_T> {
-        this.provider = protocol.setProvider(this.provider) as any;
-        return this as unknown as ProtocolCollector<NEW_T>;
+    use(protocol: { setProvider: (provider: IActivitesProvider<T>) => IProtocolActivitiesProvider<T> }): ProtocolCollector<T> {
+        this.provider = protocol.setProvider(this.provider);
+        return this;
     }
 
 
